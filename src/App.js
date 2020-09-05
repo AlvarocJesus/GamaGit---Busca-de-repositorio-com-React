c679c7abdfd3bbfb9e3a2e1220ae1b8ca5f2d522
Alvaro Coelho Jesus <alvim.coelho.jesus@gmail.com>
2020-09-04 21:35:30 -0300

    Criacao do projeto e consumindo api do github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import axios from 'axios';
+
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+      .then(res => console.log(res.data));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <input className="usuarioInput" placeholder="usuario" value={usuario} onChange={e => setUsuario(e.target.value)} />
+      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    </>
   );
 }
 
