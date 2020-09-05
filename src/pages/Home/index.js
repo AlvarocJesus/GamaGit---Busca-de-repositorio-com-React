40a6bb1325330a61dfab441ae488e25d17d62a0a
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2020-09-05 12:00:04 -0300

    Tratamento dos erros, estilizacao aos elementos, botao de retorno para a home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-import * from './styled';
+import * as S from './styled';
 import './styles.css';
 
 function Home(props) {
+  const history = useHistory();
+
   const [usuario, setUsuario] = useState('');
+  const [erro, setErro] = useState(false);
 
   function handlePesquisa(){
     axios.get(`https://api.github.com/users/${usuario}/repos`)
-      .then(res => console.log(res.data));
+      .then(res => {
+        const repositories = res.data;
+        const repositoriesName = [];
+        repositories.map((repository) => {
+          repository.push(repository.name)
+        });
+        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+        history.push('/repositories');
+      })
+      .catch(err => {
+        setErro(true);
+      });
   }
 
   return (
-
-    <Container>
-      <Input className="usuarioInput" placeholder="usuario" value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <Button type="button" onClick={handlePesquisa}>Pesquisar</Button>
-    </Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="usuario" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      { erro ? <S.ErrorMsg>Usuário não encontrado</S.ErrorMsg> : '' }
+      
+    </S.HomeContainer>
   );
 }
 
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import * as S from './styled';
 
 export default function Repositories(){
+    const  history = useHistory();
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        const repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName !== null){
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else{
+            history.push('/');
+        }
+        
+    }, []);
+
     return (
         <S.Container>
             <S.Title>Repositorios</S.Title>
             <S.List>
-                <S.ListItem>repo 1</S.ListItem>
-                <S.ListItem>repo 2</S.ListItem>
-                <S.ListItem>repo 3</S.ListItem>
-                <S.ListItem>repo 4</S.ListItem>
-                <S.ListItem>repo 5</S.ListItem>
-                <S.ListItem>repo 6</S.ListItem>
-                <S.ListItem>repo 7</S.ListItem>
-                <S.ListItem>repo 9</S.ListItem>
-                <S.ListItem>repo 10</S.ListItem>
+                { repositories.map(repository => {
+                    return (
+                    <S.ListItem>Repositório: { repository }</S.ListItem>
+                    )
+                }) }
             </S.List>
+            <S.LinkHome to="/">VoltLinkr</S.LinkHome>
         </S.Container>
-        
     );
 }
